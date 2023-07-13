fbb70757ce30f16d9c10418d54f5932de81d9193
DivineDemon <supame123@gmail.com>
2023-07-14 01:09:00 +0500

    Cleanup + API! ✨🚀🔥🔨

---
+++ src/api/auth.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    register: builder.mutation({
+      query: (userData) => ({
+        url: "auth/register",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation } = api;
